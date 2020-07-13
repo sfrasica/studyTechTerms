0eea7f1a4cc1a6ea03129342c6d44e8f4a1dec74
Steven Frasica <sfrasica1@gmail.com>
2020-07-12 23:39:13 -0400

    card component updated

--- src/Card.js
+++ src/Card.js
@@ -3,6 +3,12 @@ import React from 'react'
 export default function Card() {
     return (
         <div>
+            <h1>Header</h1>
+            <h2>Topic</h2>
+
+            <h3>Description</h3>
+
+            <button >Flip</button>
             Each individual card will go here
         </div>
     )
--- src/CardsContainer.js
+++ src/CardsContainer.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
 export default function CardsContainer() {
+    
+
+    
+    
     return (
         <div>
             All of the cards will be placed here
